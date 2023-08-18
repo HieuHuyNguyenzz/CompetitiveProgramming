72a7e2aab05a58897f2579880b5431573e876a89
HieuHuyNguyenzz <135397654+HieuHuyNguyenzz@users.noreply.github.com>
2023-08-18 14:40:43 +0700

    Update FloydWarshall.cpp

--- Thư viện/8. Graph/3. Tìm đường đi ngắn nhất/FloydWarshall.cpp
+++ Thư viện/8. Graph/3. Tìm đường đi ngắn nhất/FloydWarshall.cpp
@@ -1,24 +1,22 @@
-void init_trace(vector<vector<int>> &trace) {
-    int n = trace.size();
-    for (int u = 0; u < n; u++) {
-        for (int v = 0; v < n; v++) {
-            trace[u][v] = u;
+// implement cơ bản
+for (int k = 0; k < n; ++k) {
+    for (int i = 0; i < n; ++i) {
+        for (int j = 0; j < n; ++j) {
+            d[i][j] = min(d[i][j], d[i][k] + d[k][j]); 
         }
     }
 }
 
-void floydWarshall(int n, vector<vector<long long>> &w, vector<vector<long long>> &D, vector<vector<int>> &trace) {
-    D = w;
-    init_trace(trace); // nếu cần dò đường đi
-    
-    for (int k = 0; k < n; k++) {
-        for (int u = 0; u < n; u++) {
-            for (int v = 0; v < n; v++) {
-                if (D[u][v] > D[u][k] + D[k][v]) {
-                    D[u][v] = D[u][k] + D[k][v];
-                    trace[u][v] = trace[k][v];
-                }
-            }
+// implement đối với đồ thị có trọng số âm
+for (int k = 0; k < n; ++k) {
+    for (int i = 0; i < n; ++i) {
+        for (int j = 0; j < n; ++j) {
+            if (d[i][k] < INF && d[k][j] < INF)
+                d[i][j] = min(d[i][j], d[i][k] + d[k][j]); 
         }
     }
 }
+
+// implement với đồ thị có trọng số là số thực, khi đó cần định nghĩa một giá trị EPS
+if (d[i][k] + d[k][j] < d[i][j] - EPS)
+    d[i][j] = d[i][k] + d[k][j]; 
