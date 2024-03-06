a3f5c48c8045ea749515ac90306f87cc756b1a4e
HieuHuyNguyenzz <135397654+HieuHuyNguyenzz@users.noreply.github.com>
2024-03-07 02:04:44 +0700

    .

---
+++ Solution/CSES/src/IncreasingArray.cpp
@@ -0,0 +1,22 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+int main(){
+    ios :: sync_with_stdio(false);
+	cin.tie(0); cout.tie(0);
+    int n;
+    cin>>n;
+    int a[n];
+    for(int i=0;i<n;i++){
+        cin>>a[i];
+    }
+    long long ans=0;
+    for(int i=0;i<n-1;i++){
+        if(a[i]>a[i+1]){
+            ans+=a[i]-a[i+1];
+            a[i+1]=a[i];
+        }
+    }
+    cout<<ans;
+}
